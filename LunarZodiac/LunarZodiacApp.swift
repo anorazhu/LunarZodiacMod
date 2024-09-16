52685a168e9b40330a4c10ebcefb58a70f351893
anorazhu <zhusm@bc.edu>
2024-09-16 18:14:43 -0400

    Initial Commit

---
+++ LunarZodiac/LunarZodiacApp.swift
@@ -0,0 +1,17 @@
+//
+//  LunarZodiacApp.swift
+//  LunarZodiac
+//
+//  Created by Anora Zhu on 9/16/24.
+//
+
+import SwiftUI
+
+@main
+struct LunarZodiacApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LunarZodiacTests/LunarZodiacTests.swift
@@ -0,0 +1,17 @@
+//
+//  LunarZodiacTests.swift
+//  LunarZodiacTests
+//
+//  Created by Anora Zhu on 9/16/24.
+//
+
+import Testing
+@testable import LunarZodiac
+
+struct LunarZodiacTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
